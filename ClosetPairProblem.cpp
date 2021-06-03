8d9305155c1274676a33f3bda532f7328bd6b23c
KapitanMarbel <marbellaaustingyan@gmail.com>
2021-06-03 20:03:18 +0800

    added updated ver. of recursionLeft/Right, final touches

--- ClosetPairProblem.cpp
+++ ClosetPairProblem.cpp
@@ -1,114 +1,127 @@
 #include <iostream>
 #include <ctime>
-//#include <windows.h>
+#include <windows.h>
 #include <algorithm>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
-int recursionLeft(int array[], int median ,int size);
-int recursionRight(int array[], int median ,int size);
-
+int recursionLeft(int array[], int size);
+int recursionRight(int array[], int size);
 
 
 int main ()
 {
-   int size, gst;
-
-   cout<< "Enter Number of Chairs: ";
-   cin>> size;
-
-   int chairsarray[size];
-
-   cout<< "The number of chairs avalaible is: " << size << endl;
-
-   cout<< "Enter number of guests: ";
-   cin>> gst;
-
-	for (int i = 0; i < size; i++)
-	{
-		bool check;
-		int n;
-
-		if(i < gst)
+	int size, gst;
+	char choice;
+	
+	do
+	{	
+		system("CLS");	
+		cout<< "Enter Number of Chairs: ";
+		cin>> size;
+		cout<< "The number of chairs avalaible is: " << size << endl;
+	
+		cout<< "Enter number of guests: ";
+		cin>> gst;
+	   
+		int chairsarray[gst];
+		
+		for (int i = 0; i < gst; i++)
 		{
-			do
-			{
-				srand(time(0));
-				n = rand() % size + 1;
-
-				//check or number is already used:
-				check=true;
-
-				for (int j=0;j<i;j++)
-				if (n == chairsarray[j]) //if number is already used
+			bool check;
+			int n;
+	
+				do
 				{
-					check=false; //set check to false
-				    break; //no need to check the other elements of value[]
-				}
-
-			} while (!check); //loop until new, unique number is found
-
-			chairsarray[i]=n; //store the generated number in the array
+					srand(time(0));
+					n = rand() % size + 1;
+	
+					//check if number is already used:
+					check=true;
+	
+					for (int j = 0; j < i; j++)
+					{
+						if (n == chairsarray[j]) //if number is already used
+						{
+							check=false; //set check to false
+					    	break; //no need to check the other elements of value[]
+						}
+					}
+				}while (!check); //loop until new, unique number is found
+	
+				chairsarray[i]=n; //store the generated number in the array
 		}
-
-		else
+	
+		int n = sizeof(chairsarray) / sizeof(chairsarray[0]);
+		sort(chairsarray, chairsarray + n);
+		
+		int median;
+	    median = (gst/2);
+	
+		for (int i = 0; i < gst; i++)
 		{
-			chairsarray[i] = 0;
+			cout<< chairsarray[i] << " ";
 		}
-	}
-
-	int guestchairs[gst];
-
-	for (int i = 0; i < gst; i++)
-	{
-		guestchairs[i] = chairsarray[i];
-	}
-
-	int n = sizeof(guestchairs) / sizeof(guestchairs[0]);
-	sort(guestchairs, guestchairs + n);
-
-
-	int median;
-  median = (gst/2);
-  cout << "Median: " << guestchairs[median] << endl;
-
-	for (int i = 0; i < gst; i++)
-	{
-		cout<< guestchairs[i] << " ";
-	}
-
-  cout << endl << "The closest value to the left is: " << recursionLeft(guestchairs,median,gst) << endl;
-
-  cout << endl << "The closest value to the right is: " << recursionRight(guestchairs,median,gst) << endl;
-
+	
+		cout<< endl;
+		  
+	  	if(recursionLeft(chairsarray,median) < recursionRight(chairsarray, gst - 1))
+	  	{
+	  		cout<< "The distance of the closest pair of chairs is: " << recursionLeft(chairsarray,median) << endl;
+	  	}
+	  
+	  	else if (recursionRight(chairsarray, gst - 1) < recursionLeft(chairsarray,median) )
+	  	{
+	  		cout<< "The distance of the closest pair of chairs is: " << recursionRight(chairsarray, gst - 1) << endl;
+	  	}
+	  
+	  	else
+	  	{
+	  		cout<< "The distance of the closest pair of chairs is: " << recursionLeft(chairsarray,median) << endl;
+	  	}
+	  	
+	  	cout<< "Would you like to try again? Press Y for YES and N for NO: ";
+	  	cin>> choice;
+	  	
+	}while (choice == 'Y' || choice == 'y');
 
 	return 0;
 }
 
-int recursionLeft(int array[], int median ,int size)
+int recursionLeft(int array[], int size)
 {
-
-  if(median < size)
-  {
-    return (array[1] - array[0]);
-  }
-  else
-  {
-    return array[size] - recursionLeft(array, median ,size - 1);
-  }
-
+	int result;
+  
+	if(size > 0)
+	{
+  		int diff = abs(array[size] - array[size-1]);
+  		result = min(diff, recursionLeft(array, size - 1));
+  	}
+  
+  	else
+  	{
+    	return (array[1] - array[0]);
+  	}
+  
+  	return result;
 }
 
-int recursionRight(int array[], int median ,int size)
+int recursionRight(int array[], int size)
 {
-
-  if(median > size)
-  {
-    return (array[0] - array[1]);
-  }
-  else
-  {
-    return array[median] - recursionLeft(array, median + 1,size);
-  }
-
+	int result;
+  
+  	if(size > 0)
+  	{
+  		int diff = abs(array[size] - array[size-1]);
+  		result = min(diff, recursionLeft(array, size - 1));
+  	}
+  
+  	else
+  	{
+    	return (array[size] - array[size + 1]);
+  	}
+  
+ 	return result;
 }
