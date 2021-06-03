f5f84339474327d3955ede5f30bf32ff5a61e35b
Radorikiri <janairickindefonso@gmail.com>
2021-06-03 16:26:17 +0800

    array randomizer

--- ClosetPairProblem.cpp
+++ ClosetPairProblem.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+
+//int randRange (int low, int high)
+//{
+ //   return rand() % (high - low) + low;
+//}
 int main () 
 {
+   int size;
+   
+   cout << "Enter Array Size: ";
+   cin >> size;
+   cout << "The size of the array is: " << size;
+   	
 	
+	//srand(time(NULL));
+    //cout << randRange(1, 18);	
 	
 	
 	
